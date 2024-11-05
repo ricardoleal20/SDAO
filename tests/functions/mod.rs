33da4bed84ff5e909b29d156a940e6d21c572093
ricardoleal20 <rick.leal420@gmail.com>
2024-11-04 21:17:27 -0600

    🧪 Tests: Add tests for different benchmark functions for optimization algorithms

--- tests/functions/mod.rs
+++ tests/functions/mod.rs
@@ -5,10 +5,72 @@ use ndarray::Array1;
 /// Sphere Function: f(x) = sum(x_i^2)
 ///
 /// **Arguments**:
-///     - `x`: A reference to an Array1<f64> representing the position vector.
+///     - x: A reference to an Array1<f64> representing the position vector.
 ///
 /// **Returns**:
 ///     - `f64`: The value of the sphere function at the given position.
 pub fn sphere_function(x: &Array1<f64>) -> f64 {
     x.iter().map(|xi| xi.powi(2)).sum()
+}
+
+/// Rosenbrock Function, using a=1 and b=100:
+///     f(x) = sum_{i=1}^{d-1} [100*(x_{i+1} - x_i^2)^2 + (1 - x_i)^2]
+///
+/// **Arguments**:
+///     -x: A reference to an Array1<f64> representing the position vector.
+///
+/// **Returns**:
+///     - `f64``: The value of the Rosenbrock function at the given position.
+pub fn rosenbrock_function(x: &Array1<f64>) -> f64 {
+    x.windows(2)
+        .into_iter()
+        .map(|w| 100.0 * (w[1] - w[0].powi(2)).powi(2) + (1.0 - w[0]).powi(2))
+        .sum()
+}
+
+/// Rastrigin Function: f(x) = 10d + sum_{i=1}^{d} [x_i^2 - 10*cos(2*pi*x_i)]
+///
+/// **Arguments**:
+///     - x: A reference to an Array1<f64> representing the position vector.
+///
+/// **Returns**:
+///     - `f64`: The value of the Rastrigin function at the given position.
+pub fn rastrigin_function(x: &Array1<f64>) -> f64 {
+    let d = x.len() as f64;
+    10.0 * d
+        + x.iter()
+            .map(|xi| xi.powi(2) - 10.0 * (2.0 * std::f64::consts::PI * xi).cos())
+            .sum::<f64>()
+}
+
+/// Ackley Function:
+/// f(x) = -20*exp(-0.2*sqrt(1/d * sum(x_i^2))) - exp(1/d * sum(cos(2*pi*x_i))) + 20 + e
+///
+/// **Arguments**:
+///     - x: A reference to an Array1<f64> representing the position vector.
+///
+/// **Returns**:
+///     - `f64`: The value of the Ackley function at the given position.
+pub fn ackley_function(x: &Array1<f64>) -> f64 {
+    let d = x.len() as f64;
+    let sum_sq = x.iter().map(|xi| xi.powi(2)).sum::<f64>();
+    let sum_cos = x
+        .iter()
+        .map(|xi| (2.0 * std::f64::consts::PI * xi).cos())
+        .sum::<f64>();
+
+    -20.0 * (-0.2 * (sum_sq / d).sqrt()).exp() - (sum_cos / d).exp() + 20.0 + std::f64::consts::E
+}
+
+/// Schwefel Function: f(x) = 418.9829*d - sum_{i=1}^{d} [x_i * sin(sqrt(|x_i|))]
+///
+/// **Arguments**:
+///     - x: A reference to an Array1<f64> representing the position vector.
+///
+/// **Returns**:
+///     - f64`:The value of the Schwefel function at the given position.
+pub fn schwefel_function(x: &Array1<f64>) -> f64 {
+    let d = x.len() as f64;
+    let sum = x.iter().map(|xi| xi * (xi.abs().sqrt()).sin()).sum::<f64>();
+    418.9829 * d - sum
 }
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,30 +1,155 @@
 mod functions;
-// Use imports
-use functions::sphere_function;
+// External crate imports
+use ndarray::Array1;
+// Local imports
+use functions::{
+    ackley_function, rastrigin_function, rosenbrock_function, schwefel_function, sphere_function,
+};
 use sdao::{Status, SDAO};
+
+fn create_sdao(objective_func: fn(&Array1<f64>) -> f64, dimensions: usize) -> SDAO {
+    SDAO::new(
+        50,
+        dimensions,
+        [-10.0, 10.0].to_vec(),
+        0.01,
+        0.5,
+        1.0,
+        0.01,
+        500,
+        objective_func,
+        None,
+        None,
+        None,
+    )
+}
+
+/// SDAO initializer test. This test ensures that the SDAO can be initialized
+/// with the given parameters and that the particles are correctly created.
+#[test]
+fn test_init_sdao() {
+    // Just init the SDAO. Use the sphere function as a test
+    create_sdao(sphere_function, 1);
+}
 
 /// Simple test to ensure that the SDAO is working and is able
 /// to find the optimal solution for the sphere function.
 #[test]
 fn test_sdao_optimal_sphere() {
+    let mut optimizer = create_sdao(sphere_function, 1);
+    // Get the best particle and the status of the optimization
+    let (status, best_particle) = optimizer.run();
+    assert!(matches!(status, Status::Optimal | Status::Feasible));
+    // Check if the value for the best particle fits the expected threshold
+    if let Some(particle) = best_particle {
+        assert!(particle.best_value < 1e-6);
+    }
+}
+
+/// Simple test to ensure that the SDAO can solve the rosenbrock function
+#[test]
+fn test_sdao_optimal_rosenbrock() {
+    //let mut optimizer = create_sdao(rosenbrock_function, 2);
     let mut optimizer = SDAO::new(
         50,
-        2,
+        1,
+        [-500.0, 500.0].to_vec(),
         0.01,
         0.5,
         1.0,
         0.01,
-        1000,
-        sphere_function,
+        500,
+        rosenbrock_function,
         None,
         None,
         None,
     );
-
     // Get the best particle and the status of the optimization
     let (status, best_particle) = optimizer.run();
     assert!(matches!(status, Status::Optimal | Status::Feasible));
+    println!("Status: {:?}", status);
+    // Check if the value for the best particle fits the expected threshold
+    if let Some(particle) = best_particle {
+        assert!(particle.best_value < 1e-6);
+    }
+}
 
+/// Simple test to ensure that the SDAO can solve the rastrigin function
+#[test]
+fn test_sdao_optimal_rastrigin() {
+    //let mut optimizer = create_sdao(rastrigin_function, 1);
+    let mut optimizer = SDAO::new(
+        50,
+        1,
+        [-5.12, 5.12].to_vec(),
+        0.01,
+        0.5,
+        1.0,
+        0.01,
+        500,
+        rastrigin_function,
+        None,
+        None,
+        None,
+    );
+    // Get the best particle and the status of the optimization
+    let (status, best_particle) = optimizer.run();
+    assert!(matches!(status, Status::Optimal | Status::Feasible));
+    // Check if the value for the best particle fits the expected threshold
+    if let Some(particle) = best_particle {
+        assert!(particle.best_value < 1e-6);
+    }
+}
+
+/// Simple test to ensure that the SDAO can solve the rastrigin function
+#[test]
+fn test_sdao_optimal_ackley() {
+    // let mut optimizer = create_sdao(ackley_function, 1);
+    let mut optimizer = SDAO::new(
+        50,
+        1,
+        [-10.0, 10.0].to_vec(),
+        0.01,
+        0.5,
+        1.0,
+        0.01,
+        500,
+        ackley_function,
+        None,
+        None,
+        None,
+    );
+    // Get the best particle and the status of the optimization
+    let (status, best_particle) = optimizer.run();
+    assert!(matches!(status, Status::Optimal | Status::Feasible));
+    println!("Status: {:?}", status);
+    // Check if the value for the best particle fits the expected threshold
+    if let Some(particle) = best_particle {
+        assert!(particle.best_value < 1e-3);
+    }
+}
+
+/// Simple test to ensure that the SDAO can solve the schwefel function
+#[test]
+fn test_sdao_optimal_schwefel() {
+    let mut optimizer = SDAO::new(
+        50,
+        1,
+        [-500.0, 500.0].to_vec(),
+        0.01,
+        0.5,
+        1.0,
+        0.01,
+        500,
+        schwefel_function,
+        None,
+        None,
+        None,
+    );
+    // Get the best particle and the status of the optimization
+    let (status, best_particle) = optimizer.run();
+    assert!(matches!(status, Status::Optimal | Status::Feasible));
+    println!("Status: {:?}", status);
     // Check if the value for the best particle fits the expected threshold
     if let Some(particle) = best_particle {
         assert!(particle.best_value < 1e-6);
