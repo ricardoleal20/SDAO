91fbdec9c7a29696d09407f4d9990e26fc6e800f
ricardoleal20 <rick.leal420@gmail.com>
2024-11-10 23:56:16 -0600

    🚧 WIP: Add the other algorithms section, where I implement those other algorithms to compare it against the SDAO

--- src/benchmarks/mod.rs
+++ src/benchmarks/mod.rs
@@ -4,6 +4,7 @@ use rayon::iter::ParallelIterator;
 // Import the benchmarks functions and utilities
 mod benchmark_utils;
 mod functions;
+mod other_algorithms;
 
 use benchmark_utils::{
     generate_parameter_combinations, run_benchmark_multiple, save_benchmark_results,
@@ -12,6 +13,9 @@ use benchmark_utils::{
 use functions::{
     ackley_function, rastrigin_function, rosenbrock_function, schwefel_function, sphere_function,
 };
+// Algorithms imports
+use other_algorithms::{fick_law_algorithm, gradient_descent, simulated_annealing};
+use sdao::SDAO;
 
 /// Perform the benchmarks for the sensitivity analysis of the parameters
 ///
@@ -142,7 +146,7 @@ pub fn single_test_benchmark() {
     let beta_value = 0.01;
     let num_particles_value = 100;
     let num_threads_value: usize = 1;
-    let max_iterations = 500;
+    let max_iterations: usize = 500;
 
     // Define the functions with their search-space parameters
     let benchmark_functions: Vec<(&str, fn(&ndarray::Array1<f64>) -> f64, Vec<f64>)> = vec![
@@ -183,4 +187,148 @@ pub fn single_test_benchmark() {
     } else {
         println!("Benchmarking completed. Results saved in '{}'.", filename);
     }
+}
+
+/// Test the other algorithms agaisnt the SDAO.
+///
+/// This would test the FLA, GD, SA against the SDAO algorithm to see how they perform
+/// with the same functions.
+pub fn compare_algorithms_benchmark() {
+    // General variables
+    let max_iterations = 500;
+    let repetitions = 100; // No. of repetitions to make with each test
+
+    let benchmark_functions: Vec<(&str, fn(&ndarray::Array1<f64>) -> f64, Vec<f64>)> = vec![
+        ("Sphere", sphere_function, vec![-5.12, 5.12]),
+        ("Rosenbrock", rosenbrock_function, vec![-5.0, 10.0]),
+        ("Rastrigin", rastrigin_function, vec![-5.12, 5.12]),
+        ("Ackley", ackley_function, vec![-32.768, 32.768]),
+        ("Schwefel", schwefel_function, vec![-500.0, 500.0]),
+    ];
+
+    // * SDAO
+    // Define the default values for the SDAO.
+    let alpha_value = 0.01;
+    let gamma_value = 0.5;
+    let diff_value = 1.0;
+    let beta_value = 0.01;
+    let num_particles_value = 100;
+
+    // Run the SDAO for all the functions
+    println!("Running the SDAO algorithm for the functions...");
+    print!("");
+    for (func_name, func, range_values) in benchmark_functions.iter() {
+        let mut results: Vec<(f64, usize, f64)> = Vec::new();
+        // Run the experiments
+        for _ in 0..repetitions {
+            // Set the optimizer
+            let mut optimizer = SDAO::new(
+                num_particles_value,
+                1,
+                range_values.clone(),
+                alpha_value,
+                gamma_value,
+                diff_value,
+                beta_value,
+                max_iterations,
+                *func,
+                // Optional ones
+                None,
+                None,
+                None,
+            );
+            // Using the optimizer, run it and get the result
+            let result = optimizer.run();
+            let iterations = optimizer.used_iterations;
+            let time = optimizer.execution_time;
+            if let Some(best_particle) = result.1 {
+                results.push((best_particle.best_value, iterations, time));
+            }
+        }
+        // Get the mean values and print them
+        calculate_mean_values(func_name, results);
+    }
+
+    // * FLA
+    println!("=====================================");
+    println!("Running the FLA algorithm for the functions...");
+    print!("");
+    for (func_name, func, range_values) in benchmark_functions.iter() {
+        let mut results: Vec<(f64, usize, f64)> = Vec::new();
+        // Run the experiments
+        for _ in 0..repetitions {
+            let (best_value, iterations, time) = fick_law_algorithm(
+                num_particles_value,
+                max_iterations,
+                diff_value,
+                range_values.clone(),
+                *func,
+            );
+
+            // At the end, push the results
+            results.push((best_value, iterations as usize, time));
+        }
+        // Get the mean values and print them
+        calculate_mean_values(func_name, results);
+    }
+    // For FLA, I'll use the same number of particles and the same diff value.
+    // * SA
+    // I'll use the temperature and cooling rate inside the SA implementation
+    println!("=====================================");
+    println!("Running the SA algorithm for the functions...");
+    print!("");
+    for (func_name, func, range_values) in benchmark_functions.iter() {
+        let mut results: Vec<(f64, usize, f64)> = Vec::new();
+        // Run the experiments
+        for _ in 0..repetitions {
+            let (best_value, iterations, time) = simulated_annealing(range_values.clone(), *func);
+
+            // At the end, push the results
+            results.push((best_value, iterations as usize, time));
+        }
+        // Get the mean values and print them
+        calculate_mean_values(func_name, results);
+    }
+    // * GD
+    println!("=====================================");
+    println!("Running the GD algorithm for the functions...");
+    print!("");
+    for (func_name, func, range_values) in benchmark_functions.iter() {
+        let mut results: Vec<(f64, usize, f64)> = Vec::new();
+        // Run the experiments
+        for _ in 0..repetitions {
+            let (best_value, iterations, time) =
+                gradient_descent(range_values.clone(), alpha_value, max_iterations, *func);
+
+            // At the end, push the results
+            results.push((best_value, iterations as usize, time));
+        }
+        // Get the mean values and print them
+        calculate_mean_values(func_name, results);
+    }
+}
+
+fn calculate_mean_values(func_name: &&str, values: Vec<(f64, usize, f64)>) {
+    // Init the variables to sum the positions
+    let mut sum_best_value = 0.0;
+    let mut sum_iterations = 0;
+    let mut sum_time = 0.0;
+
+    // Iterate over the values to sum them
+    for (best_value, iterations, time) in &values {
+        sum_best_value += best_value;
+        sum_iterations += *iterations;
+        sum_time += time;
+    }
+    let len_values = values.len() as f64;
+    // Get the average
+    let avg_best_value = sum_best_value / len_values;
+    let avg_iterations = (sum_iterations as f64 / len_values).round() as usize;
+    let avg_time = sum_time / len_values;
+
+    // Print the valued
+    println!(
+        "Function: '{}'. Best value: {}. Iterations: {}. Time: {}",
+        func_name, avg_best_value, avg_iterations, avg_time
+    );
 }
---
+++ src/benchmarks/other_algorithms/mod.rs
@@ -0,0 +1,12 @@
+/// Other algorithms to test and benchmark them.
+///
+/// This algorithms work with different approaches to solve optimization problems.
+/// and it would be great to compare them with the SDAO development.
+// Import the modules here
+mod fla;
+mod gd;
+mod sa;
+// Import their algorithms here
+pub use fla::fick_law_algorithm;
+pub use gd::gradient_descent;
+pub use sa::simulated_annealing;
